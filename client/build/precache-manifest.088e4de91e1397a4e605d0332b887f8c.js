9e87c7ffe3e2fab2a070beb8dd2bd0b99fc8d433
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-12-07 23:44:59 -0800

    moved files

---
+++ client/build/precache-manifest.088e4de91e1397a4e605d0332b887f8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd38d7fa21ee197b16cc84b49b803f89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c26404c69d86e723b9c",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "879c055498c55bcdfd32",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "7c26404c69d86e723b9c",
+    "url": "/static/js/2.1797b3e0.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.1797b3e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879c055498c55bcdfd32",
+    "url": "/static/js/main.91a520a0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.91a520a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
